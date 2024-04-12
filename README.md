b47d4d47316db54d776c60fdf086ab8f14aa954c
Aquino Alejandro <aquinoalejandro666@gmail.com>
2024-04-12 10:57:59 -0300

    Fin del tp

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# python-tp2
-subir datos a una base de datos, extraer datos de una base de datos y guardarlas en csv
+# EXTRAER DATOS DE UNA BASE DE DATOS MARIADB CON PYTHON
+Para instalar utiliza pip install mariadb, y corrige el con tus datos la conexion en main.py.
+
+
+## Utilidad
+    Este programa importa los datos de localidades.csv y sube los datos a una base de datos mariadb, luego desde la base de datos extrae los datos por provincia y crea archivos correspondientes a cada provincia por su nombre con csv.
+
+
--- main.py
+++ main.py
@@ -1,44 +1,63 @@
-from ast import parse
-import csv 
+import csv
 import mariadb
-import os
 
 
+# CreO la base de datos localidades
 def createdb():
-    conn = mariadb.connect(
-        user="root",
-        password="root",
-        host="localhost"
-    )
+    conn = mariadb.connect(user="root", password="root", host="localhost")
     cursor = conn.cursor()
     cursor.execute("CREATE DATABASE IF NOT EXISTS localidades")
     cursor.execute("USE localidades")
     cursor.execute("DROP TABLE IF EXISTS provincias")
-    cursor.execute("CREATE TABLE provincias(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,nombre VARCHAR(255), localidad VARCHAR(255), id_provincia INT, codigo_postal INT, id_prov_mstr INT)")
+    cursor.execute(
+        "CREATE TABLE provincias(provincia VARCHAR(255), id INT, localidad VARCHAR(255), cp INT, id_prov_mstr INT)"
+    )
+
+        #leo datos e inserto los datos en la tabla 
+    with open("localidades.csv") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Saltar la primera fila si contiene los nombres de las columnas
+        for row in csv_reader:
+            # Verificar cada valor en la fila
+            row = [value if value != '' else '0' for value in row]  # Reemplaza '' por '0'
+            sql = "INSERT INTO provincias (provincia, id, localidad, cp, id_prov_mstr) VALUES  (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, row)
     conn.commit()
-    cursor.close()
-    conn.close()
+
+    return conn, cursor
+
+# Traigo el cursor para usarlo en otras partes del codigo
+conn, cursor = createdb()
 
 
-createdb()
-
-def insertar():
-    with open('localidades.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+def crearArchivos():
+    with open("localidades.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
         provincias_list = []
         for row in csv_reader:
             provincias_list.append(row[0])
-    provincias_list.remove('provincia')
+    provincias_list.remove("provincia")
     provincias_cleaned = sorted(set(provincias_list))
-    
+
     for provincia in provincias_cleaned:
-        with open((str(provincia)+'.csv'), 'w', encoding='utf8') as csv_file:
-            pass
+        sql = "SELECT * FROM provincias WHERE provincia = '%s';"
+        cursor.execute(sql % provincia)
 
-insertar()
+        # Obtener el resultado de la consulta
+        datos = cursor.fetchall()
+        print(provincia, datos)
 
+        with open((str(provincia) + ".csv"), "w", encoding="utf8") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=",")
+            csv_writer.writerow([i[0] for i in cursor.description])
+            csv_writer.writerows(datos)
 
+#Funcion para ejecutar el programa
+def main():
+    createdb()
+    crearArchivos()
+    conn.close()
 
+#Ejecutamos la funcion
+main()
 
-
-            
